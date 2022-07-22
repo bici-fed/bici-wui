616bb2f982cd02af067c64f8ee69c9915f0250da
jufeng <jufeng@bicitech.cn>
2022-07-22 18:40:36 +0800

    fix: 修改components

--- packages/components/dist/components.min.js
+++ packages/components/dist/components.min.js
@@ -26334,10 +26334,10 @@
           'use strict';
           n.d(e, { i: () => r });
           var r = {
-            '@bici-wui/antx': '1.0.6',
-            '@bici-wui/bus-components': '1.0.6',
-            '@bici-wui/components': '1.0.0',
-            '@bici-wui/utils': '1.0.2',
+            '@bici-wui/antx': '1.0.7',
+            '@bici-wui/bus-components': '1.0.7',
+            '@bici-wui/components': '1.0.1',
+            '@bici-wui/utils': '1.0.3',
           };
         },
         54730: (t, e, n) => {
--- packages/components/lib/version.js
+++ packages/components/lib/version.js
@@ -5,9 +5,9 @@ Object.defineProperty(exports, '__esModule', {
 });
 exports.version = void 0;
 var version = {
-  '@bici-wui/antx': '1.0.6',
-  '@bici-wui/bus-components': '1.0.6',
-  '@bici-wui/components': '1.0.0',
-  '@bici-wui/utils': '1.0.2',
+  '@bici-wui/antx': '1.0.7',
+  '@bici-wui/bus-components': '1.0.7',
+  '@bici-wui/components': '1.0.1',
+  '@bici-wui/utils': '1.0.3',
 };
 exports.version = version;
