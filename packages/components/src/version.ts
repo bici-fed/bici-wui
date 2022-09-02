d9e20c7b4003e4d58074209153bc26a506163bc7
jufeng <jufeng@bicitech.cn>
2022-09-02 10:45:18 +0800

    fix: 修改components package.json

--- packages/components/src/version.ts
+++ packages/components/src/version.ts
@@ -1,6 +1,6 @@
 export const version = {
   "@bici-wui/antx": "1.1.1",
   "@bici-wui/bus-components": "1.1.1",
-  "@bici-wui/components": "1.1.2",
+  "@bici-wui/components": "1.1.3",
   "@bici-wui/utils": "1.1.0",
 };
