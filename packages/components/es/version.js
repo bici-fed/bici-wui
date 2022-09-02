e09fb5186eb2f348674c50656900e87bf2295f33
jufeng <jufeng@bicitech.cn>
2022-09-02 10:44:02 +0800

    fix: 重新发布

--- packages/components/es/version.js
+++ packages/components/es/version.js
@@ -1,6 +1,6 @@
 export var version = {
   "@bici-wui/antx": "1.1.1",
   "@bici-wui/bus-components": "1.1.1",
-  "@bici-wui/components": "1.1.1",
+  "@bici-wui/components": "1.1.2",
   "@bici-wui/utils": "1.1.0"
 };
--- packages/components/lib/version.js
+++ packages/components/lib/version.js
@@ -7,7 +7,7 @@ exports.version = void 0;
 var version = {
   "@bici-wui/antx": "1.1.1",
   "@bici-wui/bus-components": "1.1.1",
-  "@bici-wui/components": "1.1.1",
+  "@bici-wui/components": "1.1.2",
   "@bici-wui/utils": "1.1.0"
 };
 exports.version = version;
